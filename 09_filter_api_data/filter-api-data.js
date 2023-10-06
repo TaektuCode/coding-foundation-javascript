3e3ca8d04acfaa7bff6d5e05a719e4eee924e048
taektu_coding <taektu.coding@gmail.com>
2023-10-06 13:46:00 +0200

    update kata filter api data

--- 09_filter_api_data/filter-api-data.js
+++ 09_filter_api_data/filter-api-data.js
@@ -2,7 +2,7 @@ function filterApiData(apiData, mandatoryKeys) {
   // filter methode zum erstellen eines neuen Objects welches alle "mandatory keys" enthält
 
   return apiData.filter((obj) => {
-    // Check if every mandatory key is present in the object
+    // checken ob jeder mandatoryKey vorhanden ist.
     return mandatoryKeys.every((key) => obj.hasOwnProperty(key));
   });
 }
